07d69a8e6644c0d7312a0aa075023460a138f4ca
Hassan Ali <mhasan2495@gmail.com>
2025-12-02 13:15:21 -0500

    app constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants{
+  // WebView URLs
+  static const String webviewUrlLocalhost = 'http://localhost:4200';
+  static const String webviewUrlAndroidEmulator = 'http://10.0.2.2:4200';
+
+  // Database
+  static const String databaseName = 'messages.db';
+  static const int databaseVersion = 1;
+
+}
